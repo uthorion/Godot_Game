af4c02c62ba72e6f5a68b69d6d13810983c57c1a
felix <felixagustinbengoa@gmail.com>
2025-05-09 10:09:59 -0300

    agregado un menu provisional, hubo problemas para aplicar bien el tema del gestor de texto por lo que lo solucionare despues

--- juego_aventura_grafica/Scrips/dialogo.gd
+++ juego_aventura_grafica/Scrips/dialogo.gd
@@ -2,17 +2,15 @@ extends Control
 
 @onready var rich_text = $MarginContainer/Dialogos
 
-var dialogos = [
-	"Hola, ¿cómo estás?",
-	"Hace mucho que no nos veíamos...",
-	"¿Recuerdas la vieja casa donde crecimos?",
-	"Tengo algo importante que contarte."
-]
+var dialogos = []
 var current_dialog_index = 0
 var escribiendo = false
 var texto_completo = ""
 
-func _ready():
+func mostrar_dialogo_externo(nuevos_dialogos: Array):
+	dialogos = nuevos_dialogos
+	current_dialog_index = 0
+	visible = true
 	rich_text.clear()
 	mostrar_dialogo()
 
@@ -22,29 +20,27 @@ func mostrar_dialogo():
 		current_dialog_index += 1
 		escribir_texto(texto_completo)
 	else:
+		visible = false  # Oculta el panel al terminar
 		print("Fin del diálogo")
-		
+
 func escribir_texto(texto):
 	$MarginContainer/Dialogos.clear()
 	escribiendo = true
 	await mostrar_texto_letra_por_letra(texto)
 	escribiendo = false
 
-
-func _on_control_gui_input(event):
+func _on_Control_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if escribiendo:
-			# Si el jugador hace clic mientras se escribe, mostrar el texto completo al instante
 			$MarginContainer/Dialogos.text = texto_completo
 			escribiendo = false
 		else:
 			mostrar_dialogo()
-			
+
 func mostrar_texto_letra_por_letra(texto: String) -> void:
 	rich_text.clear()
 	for i in texto.length():
 		if not escribiendo:
-			# Si se interrumpe (por clic), salir
 			return
 		rich_text.append_text(texto[i])
-		await get_tree().create_timer(0.03).timeout  # velocidad de escritura
+		await get_tree().create_timer(0.03).timeout
---
+++ juego_aventura_grafica/Scrips/main.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+func _on_jugar_pressed():
+	get_tree().change_scene_to_file("res://Scenes/map_00.tscn")
+
+func _on_salir_pressed():
+	get_tree().quit()
